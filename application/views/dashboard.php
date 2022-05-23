a3d13d94362685dc667a8531c3268ea6c415a177
marvinraydalida <marvinraydalida@gmail.com>
2022-05-23 23:00:11 +0800

    Fixed overflow scroll in request table

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,40 +6,103 @@
 
         <div id="request-container">
             <h1>Appointment request (row count ng mga pending request)</h1>
-            <table>
-                <tr>
-                    <th>Time</th>
-                    <th>Date</th>
-                    <th>Name</th>
-                    <th>Contact</th>
-                    <th>Email</th>
-                    <th>Action</th>
-                </tr>
-                <tr>
-                    <td style="font-weight: bold;">7:00 am</td>
-                    <td>2022-05-24</td>
-                    <td>Alfreds Futterkiste</td>
-                    <td>09972184815</td>
-                    <td>marvinraydalida@gmail.com</td>
-                    <td>
-                        <button>Accept</button>
-                        <button>Reschedule</button>
-                        <button>Decline</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="font-weight: bold;">7:00 am</td>
-                    <td>2022-05-24</td>
-                    <td>Alfreds Futterkiste</td>
-                    <td>09972184815</td>
-                    <td>marvinraydalida@gmail.com</td>
-                    <td>
-                        <button>Accept</button>
-                        <button>Reschedule</button>
-                        <button>Decline</button>
-                    </td>
-                </tr>
-            </table>
+
+            <div id="table-container">
+                <table>
+                    <tr>
+                        <th>Time</th>
+                        <th>Date</th>
+                        <th>Name</th>
+                        <th>Contact</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">7:00 am</td>
+                        <td>2022-05-24</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>09972184815</td>
+                        <td>marvinraydalida@gmail.com</td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Reschedule</button>
+                            <button>Decline</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
 
         </div>
     </section>
