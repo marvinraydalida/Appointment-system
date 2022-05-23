a9ee10c415520ab5b842077367065174a18ca101
marvinraydalida <marvinraydalida@gmail.com>
2022-05-23 22:48:52 +0800

    Added appointments

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,14 +1,49 @@
-    <section id = "dashboard">
-        <div id = "analytics-container">
-            <div id = "weekly-graph-container"></div>
-            <div id = "request-graph-container"></div>
+    <section id="dashboard">
+        <div id="analytics-container">
+            <div id="weekly-graph-container"></div>
+            <div id="request-graph-container"></div>
         </div>
 
-        <div id = "request-container">
-            
+        <div id="request-container">
+            <h1>Appointment request (row count ng mga pending request)</h1>
+            <table>
+                <tr>
+                    <th>Time</th>
+                    <th>Date</th>
+                    <th>Name</th>
+                    <th>Contact</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold;">7:00 am</td>
+                    <td>2022-05-24</td>
+                    <td>Alfreds Futterkiste</td>
+                    <td>09972184815</td>
+                    <td>marvinraydalida@gmail.com</td>
+                    <td>
+                        <button>Accept</button>
+                        <button>Reschedule</button>
+                        <button>Decline</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold;">7:00 am</td>
+                    <td>2022-05-24</td>
+                    <td>Alfreds Futterkiste</td>
+                    <td>09972184815</td>
+                    <td>marvinraydalida@gmail.com</td>
+                    <td>
+                        <button>Accept</button>
+                        <button>Reschedule</button>
+                        <button>Decline</button>
+                    </td>
+                </tr>
+            </table>
+
         </div>
     </section>
-	
-</body>
 
-</html>
+    </body>
+
+    </html>
