030b4582dcdec4c18d5f2c404c73383187b285b0
William <williamcris18@gmail.com>
2022-05-31 02:51:29 +0800

    Modified email message design

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -28,62 +28,45 @@ class Admin extends CI_Controller
 
     public function sendEmailAccepted($id)
     {
-        $this->Appointment_model->acceptAppointment($id);
+        // $this->Appointment_model->acceptAppointment($id);
         $this->load->library('email');
         $this->load->config('email');
         $this->email->set_mailtype("html");
 
         $data = $this->Appointment_model->viewSingleAppointment($id);
-        $message = '<!DOCTYPE><html><head></head><body><h1>Good day! Naaccept na yung appointment mong bwakanangshit ka, 
-        eto details mong kupal ka:
-        </h1><p> Name:' . $data->name . '</p>
-        <p> Age:' . $data->age . '</p>
-        <p> Sex:' . $data->sex . '</p>
-        <p> Address:' . $data->address . '</p>
-        <p> Contact:' . $data->contact . '</p>
-        <p> Email:' . $data->email . '</p>
-        <p> Date of appointment:' . date("m/d/Y", strtotime($data->date)) . '</p>
-        <p> Time of appointment:' . date('h:i a', strtotime($data->time)) . '</p></body></html>';
+       
         $this->email->from($this->config->item('smtp_user'));
         $this->email->to($data->email);
-
+        $message = $this->load->view('templates/email_templateAccepted',$data,TRUE);
         $this->email->subject('Email Test');
         $this->email->message($message);
         if ($this->email->send()) {
-            echo ("Email sent");
-            redirect('Admin');
-        } else {
+             redirect('Admin');
+            
+         } else {
             show_error($this->email->print_debugger());
         }
     }
     
     public function sendEmailDeclined($id)
     {
-        $this->Appointment_model->declineAppointment($id);
+        // $this->Appointment_model->declineAppointment($id);
         $this->load->library('email');
         $this->load->config('email');
         $this->email->set_mailtype("html");
 
         $data = $this->Appointment_model->viewSingleAppointment($id);
-        $message = '<!DOCTYPE><html><head></head><body><h1>Good day! Declined yung appointment mong bwakanangshit ka, 
-        eto details mong kupal ka:
-        </h1><p> Name:' . $data->name . '</p>
-        <p> Age:' . $data->age . '</p>
-        <p> Sex:' . $data->sex . '</p>
-        <p> Address:' . $data->address . '</p>
-        <p> Contact:' . $data->contact . '</p>
-        <p> Email:' . $data->email . '</p>
-        <p> Date of appointment:' . date("m/d/Y", strtotime($data->date)) . '</p>
-        <p> Time of appointment:' . date('h:i a', strtotime($data->time)) . '</p></body></html>';
+       
         $this->email->from($this->config->item('smtp_user'));
         $this->email->to($data->email);
-
+        $message = $this->load->view('templates/email_templateDeclined',$data,TRUE);
         $this->email->subject('Email Test');
         $this->email->message($message);
+        $this->load->view('templates/email_templateDeclined',$data);
         if ($this->email->send()) {
-            echo ("Email sent");
-            redirect('Admin');
-        } else {
+             redirect('Admin');
+            
+         } else {
             show_error($this->email->print_debugger());
         }
     }
